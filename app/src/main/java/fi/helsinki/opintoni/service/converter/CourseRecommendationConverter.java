adf7e3245f2b191e51a1f9ea0876bc63c21f4b5f
Marko Rautajoki <marko.rautajoki@gofore.com>
2016-03-17 11:46:28 +0200

    Removed optional

--- app/src/main/java/fi/helsinki/opintoni/service/converter/CourseRecommendationConverter.java
+++ app/src/main/java/fi/helsinki/opintoni/service/converter/CourseRecommendationConverter.java
@@ -23,7 +23,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
-import java.util.Optional;
 
 @Component
 public class CourseRecommendationConverter {
@@ -53,9 +52,6 @@ public class CourseRecommendationConverter {
     }
 
     private String getRecommendedCourseLink(LeikiCourseRecommendation leikiCourseRecommendation) {
-        return Optional
-            .ofNullable(leikiCourseRecommendation.link)
-            .map(coursePageUriLocalizer::localize)
-            .orElse(null);
+        return leikiCourseRecommendation.link != null ? coursePageUriLocalizer.localize(leikiCourseRecommendation.link) : null;
     }
 }
