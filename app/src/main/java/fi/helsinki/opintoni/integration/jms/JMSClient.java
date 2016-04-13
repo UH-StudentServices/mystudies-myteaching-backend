e9f960401fb9ea3cc53c93d8bb24d9ed3848d914
Marko Rautajoki <marko.rautajoki@gofore.com>
2016-04-13 09:59:58 +0300

    Added logging to JMS client

--- app/src/main/java/fi/helsinki/opintoni/integration/jms/JMSClient.java
+++ app/src/main/java/fi/helsinki/opintoni/integration/jms/JMSClient.java
@@ -2,14 +2,20 @@ package fi.helsinki.opintoni.integration.jms;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public abstract class JMSClient {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(JMSClient.class);
+
     private static final int SYNC_TIMEOUT = 10000;
 
     private final JmsTemplate jmsTemplate;
@@ -38,6 +44,8 @@ public abstract class JMSClient {
     }
 
     protected String queryMethodWithParameters(String method, Map<String, String> parameters) {
+        LOGGER.info("Querying method  " + method + " synchronously over JMS with parameters " + Arrays.toString(parameters.entrySet().toArray()));
+
         try {
             return sendMessage(method, parameters).getText();
         } catch (JMSException e) {
@@ -46,6 +54,7 @@ public abstract class JMSClient {
     }
 
     protected <T> T parseResponse(String response, TypeReference<T> typeReference) {
+        LOGGER.info("Parsing single response object " + response );
         try {
             return objectMapper.readValue(getDataFromResponse(response), typeReference);
         } catch (Exception e) {
@@ -54,6 +63,7 @@ public abstract class JMSClient {
     }
 
     protected <T> List<T> parseListResponse(String response, TypeReference<List<T>> typeReference ) {
+        LOGGER.info("Parsing list of response objects " + response );
         try {
             return objectMapper.readValue(getDataFromResponse(response), typeReference);
         } catch (Exception e) {
