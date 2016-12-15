fa5adb1af4ed6dcbd7d841187114267465d50559
David Price <david.price@codento.com>
2016-12-15 12:17:10 +0200

    Add missing import

--- app/src/main/java/fi/helsinki/opintoni/service/portfolio/SampleService.java
+++ app/src/main/java/fi/helsinki/opintoni/service/portfolio/SampleService.java
@@ -29,6 +29,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
