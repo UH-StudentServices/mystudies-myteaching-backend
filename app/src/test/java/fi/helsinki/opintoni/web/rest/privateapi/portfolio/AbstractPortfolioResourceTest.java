c5b41bc9f3362dd050f0d61681df2ba23d425121
Henri Heiskanen <henri.heiskanen@gofore.com>
2017-01-27 18:39:59 +0200

    make PrivatePortfolioResourceTest great again
    (what was ever the point of splitting PrivatePortfolioResource tests across two files?)

--- app/src/test/java/fi/helsinki/opintoni/web/rest/privateapi/portfolio/AbstractPortfolioResourceTest.java
+++ app/src/test/java/fi/helsinki/opintoni/web/rest/privateapi/portfolio/AbstractPortfolioResourceTest.java
@@ -3,6 +3,7 @@ package fi.helsinki.opintoni.web.rest.privateapi.portfolio;
 import fi.helsinki.opintoni.SpringTest;
 import fi.helsinki.opintoni.config.Constants;
 import fi.helsinki.opintoni.localization.Language;
+import fi.helsinki.opintoni.web.rest.RestConstants;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.test.web.servlet.ResultActions;
@@ -14,9 +15,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 public abstract class AbstractPortfolioResourceTest extends SpringTest {
 
-    protected static final String PRIVATE_PORTFOLIO_API_PATH = "/api/private/v1/portfolio";
-    protected static final String STUDENT_PORTFOLIO_API_URL = PRIVATE_PORTFOLIO_API_PATH + "/student";
-    protected static final String TEACHER_PORTFOLIO_API_URL = PRIVATE_PORTFOLIO_API_PATH + "/teacher";
+    protected static final String PRIVATE_PORTFOLIO_API_PATH = RestConstants.PRIVATE_API_V1 + "/portfolio";
+    protected static final String STUDENT_PORTFOLIO_API_PATH = PRIVATE_PORTFOLIO_API_PATH + "/student";
+    protected static final String TEACHER_PORTFOLIO_API_PATH = PRIVATE_PORTFOLIO_API_PATH + "/teacher";
     protected static final String SESSION_LANG = Language.EN.getCode();
 
     private static final String EMPLOYEE_NUMBER = "010540";
@@ -31,19 +32,19 @@ public abstract class AbstractPortfolioResourceTest extends SpringTest {
     }
 
     protected ResultActions createStudentPortfolio(SecurityContext securityContext) throws Exception {
-        return createPortfolio(securityContext, STUDENT_PORTFOLIO_API_URL);
+        return createPortfolio(securityContext, STUDENT_PORTFOLIO_API_PATH);
     }
 
     protected ResultActions createTeacherPortfolio(SecurityContext securityContext) throws Exception {
-        return createPortfolio(securityContext, TEACHER_PORTFOLIO_API_URL);
+        return createPortfolio(securityContext, TEACHER_PORTFOLIO_API_PATH);
     }
 
     protected ResultActions createStudentPortfolio(SecurityContext securityContext, Language lang) throws Exception {
-        return createPortfolio(securityContext, String.join("/", STUDENT_PORTFOLIO_API_URL, lang.getCode()));
+        return createPortfolio(securityContext, String.join("/", STUDENT_PORTFOLIO_API_PATH, lang.getCode()));
     }
 
     protected ResultActions createTeacherPortfolio(SecurityContext securityContext, Language lang) throws Exception {
-        return createPortfolio(securityContext, String.join("/", TEACHER_PORTFOLIO_API_URL, lang.getCode()));
+        return createPortfolio(securityContext, String.join("/", TEACHER_PORTFOLIO_API_PATH, lang.getCode()));
     }
 
     protected void expectEmployeeContactInformationRequestToESB() {
