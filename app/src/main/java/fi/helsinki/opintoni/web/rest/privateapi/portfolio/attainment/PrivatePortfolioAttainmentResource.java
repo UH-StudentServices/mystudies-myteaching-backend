5710d858e1a1f10797989c2a132f10b1b918e3b0
Joonas Iivonen <joonas.iivonen@codento.com>
2017-05-08 15:15:41 +0300

    Removed extra whitespace

--- app/src/main/java/fi/helsinki/opintoni/web/rest/privateapi/portfolio/attainment/PrivatePortfolioAttainmentResource.java
+++ app/src/main/java/fi/helsinki/opintoni/web/rest/privateapi/portfolio/attainment/PrivatePortfolioAttainmentResource.java
@@ -37,9 +37,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Locale;
