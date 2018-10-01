7c6b4bb2644fbf3c4be5a2b0c163f6b63b2e4c29
Marko Rautajoki <markorautajoki@gmail.com>
2018-10-01 17:40:35 +0300

    Try chown

--- docker/ci/script/my-backend-develop-build.sh
+++ docker/ci/script/my-backend-develop-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd docker/ci
-/usr/local/bin/docker-compose run --user jenkins --entrypoint "gradle storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
+/usr/local/bin/docker-compose build --no-cache
+/usr/local/bin/docker-compose run --user jenkins --entrypoint "chown -R jenkins:jenkins /home/jenkins && gradle storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
 /usr/local/bin/docker-compose stop
 /usr/local/bin/docker-compose rm -f
