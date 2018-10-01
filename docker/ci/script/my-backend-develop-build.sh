caee1337e41184a60e86eca478268e70aa37db59
Marko Rautajoki <markorautajoki@gmail.com>
2018-10-01 19:11:09 +0300

    Remove building

--- docker/ci/script/my-backend-develop-build.sh
+++ docker/ci/script/my-backend-develop-build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd docker/ci
-/usr/local/bin/docker-compose build --no-cache
 /usr/local/bin/docker-compose run --rm -v my-backend-develop-build-gradle:/home/jenkins/.gradle -v my-backend-develop-build-m2:/home/jenkins/.m2 --user jenkins --entrypoint "gradle storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
 /usr/local/bin/docker-compose stop
 /usr/local/bin/docker-compose rm -f
