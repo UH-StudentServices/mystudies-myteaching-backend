7e841b86ed4c2b87907b402d7ab844520241fc74
Marko Rautajoki <markorautajoki@gmail.com>
2018-10-01 09:22:19 +0300

    Fix typo

--- docker/ci/script/my-backend-develop-build.sh
+++ docker/ci/script/my-backend-develop-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd docker/ci
+/usr/local/bin/docker-compose build --no-cache
 /usr/local/bin/docker-compose run -v ~/.gradle-docker/my-backend-develop-build:/jenkins/.gradle --rm --user jenkins --entrypoint "gradle storeDeployInformationToFile publish --refresh-dependencies -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
 /usr/local/bin/docker-compose stop
 /usr/local/bin/docker-compose rm -f
