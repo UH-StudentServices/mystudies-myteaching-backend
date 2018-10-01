f23fc46fb085ea68be3a6e9598be910b8f361d4a
Marko Rautajoki <markorautajoki@gmail.com>
2018-10-01 15:38:49 +0300

    Add cache container

--- docker/ci/script/my-backend-develop-build.sh
+++ docker/ci/script/my-backend-develop-build.sh
@@ -2,6 +2,6 @@
 
 cd docker/ci
 /usr/local/bin/docker-compose build --no-cache
-/usr/local/bin/docker-compose run --rm --user jenkins -v ~/.gradle-docker/my-backend-develop-build:/home/jenkins/.gradle --entrypoint "gradle storeDeployInformationToFile publish --refresh-dependencies -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
+/usr/local/bin/docker-compose run --rm --user jenkins --entrypoint "gradle storeDeployInformationToFile publish --refresh-dependencies -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
 /usr/local/bin/docker-compose stop
 /usr/local/bin/docker-compose rm -f
