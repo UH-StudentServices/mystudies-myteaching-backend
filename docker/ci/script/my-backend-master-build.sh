26c8123f8ec21ffb19dbac6d633e08f43dd93edf
Marko Rautajoki <markorautajoki@gmail.com>
2018-10-01 20:31:20 +0300

    Add -v

--- docker/ci/script/my-backend-master-build.sh
+++ docker/ci/script/my-backend-master-build.sh
@@ -3,5 +3,5 @@
 cd docker/ci
 /usr/local/bin/docker-compose run -v my-backend-master-build-gradle:/home/jenkins/.gradle -v my-backend-master-build-m2:/home/jenkins/.m2 --rm --user jenkins --entrypoint "gradle release --refresh-dependencies -Penvironment=prod -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD} -Pgradle.release.useAutomaticVersion=true" my-studies-builder
 /usr/local/bin/docker-compose stop
-/usr/local/bin/docker-compose rm -f
+/usr/local/bin/docker-compose rm -f -v
 
