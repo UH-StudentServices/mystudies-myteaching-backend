805a54c65d0f411a8eaca36a1ca36736033cb7ac
Marko Rautajoki <markorautajoki@gmail.com>
2018-10-01 18:11:28 +0300

    FIx

--- docker/ci/script/my-backend-develop-build.sh
+++ docker/ci/script/my-backend-develop-build.sh
@@ -2,6 +2,6 @@
 
 cd docker/ci
 /usr/local/bin/docker-compose build --no-cache
-/usr/local/bin/docker-compose run -v ~/.gradle-docker/my-backend-develop-build:/home/jenkins/.gradle --user jenkins --entrypoint "gradle storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
+/usr/local/bin/docker-compose run --user jenkins --entrypoint "gradle storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
 /usr/local/bin/docker-compose stop
 /usr/local/bin/docker-compose rm -f
