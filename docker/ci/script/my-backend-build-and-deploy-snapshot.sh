eb4ec59078bb45b3889050cf163d970d3f5a55c8
Marko Rautajoki <markorautajoki@gmail.com>
2018-10-02 12:17:09 +0300

    Add braces

--- docker/ci/script/my-backend-build-and-deploy-snapshot.sh
+++ docker/ci/script/my-backend-build-and-deploy-snapshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd docker/ci
-/usr/local/bin/docker-compose run -v my-backend-build-and-deploy-snapshot-gradle:/home/jenkins/.gradle -v my-backend-build-and-deploy-snapshot-m2:/home/jenkins/.m2 --rm --user jenkins --entrypoint "storeDeployInformationToFile publish -Penvironment=$ENVIRONMENT -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
+/usr/local/bin/docker-compose run -v my-backend-build-and-deploy-snapshot-gradle:/home/jenkins/.gradle -v my-backend-build-and-deploy-snapshot-m2:/home/jenkins/.m2 --rm --user jenkins --entrypoint "storeDeployInformationToFile publish -Penvironment=${ENVIRONMENT} -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
 /usr/local/bin/docker-compose stop
 /usr/local/bin/docker-compose rm -f -v
