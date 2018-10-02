5b825cab5681708ac3bda727881147a63f963f09
Marko Rautajoki <markorautajoki@gmail.com>
2018-10-02 15:38:15 +0300

    Use -p switch

--- docker/ci/script/run.sh
+++ docker/ci/script/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose run -v $1-gradle:/home/jenkins/.gradle -v $1-m2:/home/jenkins/.m2 --rm --user jenkins --entrypoint "$2" my-studies-builder
+/usr/local/bin/docker-compose -p $1 run -v $1-gradle:/home/jenkins/.gradle -v $1-m2:/home/jenkins/.m2 --rm --user jenkins --entrypoint "$2" my-studies-builder
 exit_code=$?
 /usr/local/bin/docker-compose stop
 /usr/local/bin/docker-compose rm -f -v
