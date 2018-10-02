a1c69a2c30015b98e407297f1437efb47aa46a09
Marko Rautajoki <markorautajoki@gmail.com>
2018-10-02 15:49:46 +0300

    Add -p

--- docker/ci/script/run.sh
+++ docker/ci/script/run.sh
@@ -2,6 +2,6 @@
 
 /usr/local/bin/docker-compose -p $1 run -v $1-gradle:/home/jenkins/.gradle -v $1-m2:/home/jenkins/.m2 --rm --user jenkins --entrypoint "$2" my-studies-builder
 exit_code=$?
-/usr/local/bin/docker-compose stop
-/usr/local/bin/docker-compose rm -f -v
+/usr/local/bin/docker-compose -p $1 stop
+/usr/local/bin/docker-compose -p $1 rm -f -v
 exit $exit_code
