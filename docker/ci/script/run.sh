ecc97714cdfd9e6288916cac35eb676622953e8c
Marko Rautajoki <markorautajoki@gmail.com>
2018-11-08 12:10:50 +0200

    Review remarks

--- docker/ci/script/run.sh
+++ docker/ci/script/run.sh
@@ -5,14 +5,20 @@ USER_ID="$(id -u)"
 GROUP_NAME="$(id -gn)"
 GROUP_ID="$(id -g)"
 
+PATH=$PATH:/usr/local/bin
+
 stop_and_remove_containers() {
-  /usr/local/bin/docker-compose -p $1 stop
-  /usr/local/bin/docker-compose -p $1 rm -f -v
+  docker-compose -p $1 rm -fsv
 }
 
 stop_and_remove_containers $1
-/usr/local/bin/docker-compose build --build-arg USER_NAME="$USER_NAME" --build-arg USER_ID=$USER_ID --build-arg GROUP_NAME="$GROUP_NAME" --build-arg GROUP_ID=$GROUP_ID
-/usr/local/bin/docker-compose -p $1 run -v ~/.ssh/:/home/$USER_NAME/.ssh:ro -v ~/.gitconfig:/home/$USER_NAME/.gitconfig -v $1-gradle:/home/$USER_NAME/.gradle -v $1-m2:/home/$USER_NAME/.m2 --rm --user $USER_NAME --entrypoint "$2" my-studies-builder
+docker-compose build --build-arg USER_NAME="$USER_NAME" --build-arg USER_ID=$USER_ID --build-arg GROUP_NAME="$GROUP_NAME" --build-arg GROUP_ID=$GROUP_ID
+docker-compose -p $1 run \
+-v ~/.ssh/:/home/$USER_NAME/.ssh:ro \
+-v ~/.gitconfig:/home/$USER_NAME/.gitconfig \
+-v $1-gradle:/home/$USER_NAME/.gradle \
+-v $1-m2:/home/$USER_NAME/.m2 \
+--rm --user $USER_NAME --entrypoint "$2" my-studies-builder
 exit_code=$?
 stop_and_remove_containers $1
 exit $exit_code
