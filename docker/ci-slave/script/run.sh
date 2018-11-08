f8b25a22b6d65ef484355818cd39e53d98110e35
Marko Rautajoki <markorautajoki@gmail.com>
2018-11-08 08:31:25 +0200

    Clear containers before test run. Parameterize run script to be able to use in multiple ci environments

--- docker/ci-slave/script/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/docker-compose -p $1 run -v ~/.ssh/:/home/ci/.ssh:ro -v ~/.gitconfig:/home/ci/.gitconfig -v $1-gradle:/home/ci/.gradle -v $1-m2:/home/ci/.m2 --rm --user ci --entrypoint "$2" my-studies-builder
-exit_code=$?
-/usr/local/bin/docker-compose -p $1 stop
-/usr/local/bin/docker-compose -p $1 rm -f -v
-exit $exit_code
--- docker/ci/script/run.sh
+++ docker/ci/script/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -p $1 run -v $1-gradle:/home/jenkins/.gradle -v $1-m2:/home/jenkins/.m2 --rm --user jenkins --entrypoint "$2" my-studies-builder
+USER_NAME="$(id -un)"
+USER_ID="$(id -un)"
+GROUP_NAME="$(id -gn)"
+GROUP_ID="$(id -g)"
+
+stop_and_remove_containers() {
+  /usr/local/bin/docker-compose -p $1 stop
+  /usr/local/bin/docker-compose -p $1 rm -f -v
+}
+
+stop_and_remove_containers $1
+/usr/local/bin/docker-compose build --build-arg USER_NAME=$USER_NAME --build-arg USER_ID=$USER_ID --build-arg GROUP_NAME=$GROUP_NAME --build-arg GROUP_ID=$GROUP_ID
+/usr/local/bin/docker-compose -p $1 run un -v ~/.ssh/:/home/$USER_NAME/.ssh:ro -v ~/.gitconfig:/home/$USER_NAME/.gitconfig -v $1-gradle:/home/$USER_NAME/.gradle -v $1-m2:/home/$USER_NAME/.m2 --rm --user jenkins --entrypoint "$2" my-studies-builder
 exit_code=$?
-/usr/local/bin/docker-compose -p $1 stop
-/usr/local/bin/docker-compose -p $1 rm -f -v
+stop_and_remove_containers $1
 exit $exit_code
