8daf9b6157113305045a691111783471f3a9e24a
Marko Rautajoki <markorautajoki@gmail.com>
2018-11-08 09:06:24 +0200

    Fix script

--- docker/ci/script/run.sh
+++ docker/ci/script/run.sh
@@ -11,8 +11,8 @@ stop_and_remove_containers() {
 }
 
 stop_and_remove_containers $1
-/usr/local/bin/docker-compose build --build-arg USER_NAME=$USER_NAME --build-arg USER_ID=$USER_ID --build-arg GROUP_NAME=$GROUP_NAME --build-arg GROUP_ID=$GROUP_ID
-/usr/local/bin/docker-compose -p $1 run un -v ~/.ssh/:/home/$USER_NAME/.ssh:ro -v ~/.gitconfig:/home/$USER_NAME/.gitconfig -v $1-gradle:/home/$USER_NAME/.gradle -v $1-m2:/home/$USER_NAME/.m2 --rm --user jenkins --entrypoint "$2" my-studies-builder
+/usr/local/bin/docker-compose build --build-arg USER_NAME="$USER_NAME" --build-arg USER_ID=$USER_ID --build-arg GROUP_NAME="$GROUP_NAME" --build-arg GROUP_ID=$GROUP_ID
+/usr/local/bin/docker-compose -p $1 run -v ~/.ssh/:/home/$USER_NAME/.ssh:ro -v ~/.gitconfig:/home/$USER_NAME/.gitconfig -v $1-gradle:/home/$USER_NAME/.gradle -v $1-m2:/home/$USER_NAME/.m2 --rm --user $USER_NAME --entrypoint "$2" my-studies-builder
 exit_code=$?
 stop_and_remove_containers $1
 exit $exit_code
