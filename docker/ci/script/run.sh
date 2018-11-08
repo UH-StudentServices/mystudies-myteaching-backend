597e2ccdb8c202e2f3e0eed0f62376819510823f
Marko Rautajoki <markorautajoki@gmail.com>
2018-11-08 12:42:37 +0200

    Use read only for gitconfig. Split build command to multiple lines

--- docker/ci/script/run.sh
+++ docker/ci/script/run.sh
@@ -12,10 +12,15 @@ stop_and_remove_containers() {
 }
 
 stop_and_remove_containers $1
-docker-compose build --build-arg USER_NAME="$USER_NAME" --build-arg USER_ID=$USER_ID --build-arg GROUP_NAME="$GROUP_NAME" --build-arg GROUP_ID=$GROUP_ID
+docker-compose build \
+--build-arg USER_NAME="$USER_NAME" \
+--build-arg USER_ID=$USER_ID \
+--build-arg GROUP_NAME="$GROUP_NAME" \
+--build-arg GROUP_ID=$GROUP_ID
+
 docker-compose -p $1 run \
 -v ~/.ssh/:/home/$USER_NAME/.ssh:ro \
--v ~/.gitconfig:/home/$USER_NAME/.gitconfig \
+-v ~/.gitconfig:/home/$USER_NAME/.gitconfig:ro \
 -v $1-gradle:/home/$USER_NAME/.gradle \
 -v $1-m2:/home/$USER_NAME/.m2 \
 --rm --user $USER_NAME --entrypoint "$2" my-studies-builder
