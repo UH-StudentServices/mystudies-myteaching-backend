d581017d6478844448d78557bd8d89426986bcd2
Sami Siren <siren@foofactory.fi>
2018-11-12 15:36:57 +0200

    fix typo

--- docker/ci/script/my-backend-develop-build.sh
+++ docker/ci/script/my-backend-develop-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./run.sh my-backend-develop-build "gradle --info storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Partifacory=${http://opi-1.student.helsinki.fi:8081/artifactory/remote-repos/} -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -PXopintoni_artifactory_password=${ARTIFACTORY_PASSWORD}"
+./run.sh my-backend-develop-build "gradle --info storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Partifacory=http://opi-1.student.helsinki.fi:8081/artifactory/remote-repos/ -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -PXopintoni_artifactory_password=${ARTIFACTORY_PASSWORD}"
