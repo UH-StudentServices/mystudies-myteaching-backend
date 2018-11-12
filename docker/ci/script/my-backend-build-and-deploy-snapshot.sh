d72d4cfff2b0e3716a6713c326bf44b365285cb2
Sami Siren <siren@foofactory.fi>
2018-11-12 16:09:26 +0200

    set artifactory url(for repositories)

--- docker/ci/script/my-backend-build-and-deploy-snapshot.sh
+++ docker/ci/script/my-backend-build-and-deploy-snapshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./run.sh my-backend-build-and-deploy-snapshot "storeDeployInformationToFile publish -Penvironment=${ENVIRONMENT} -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}"
+./run.sh my-backend-build-and-deploy-snapshot "storeDeployInformationToFile publish -Penvironment=${ENVIRONMENT} -Partifactory=${ARTIFACTORY_BASE_URL}/remote-repos/ -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}"
--- docker/ci/script/my-backend-publish.sh
+++ docker/ci/script/my-backend-publish.sh
@@ -3,6 +3,6 @@
 job=my-backend-publish
 build_cache_prefix=my-backend-master-build
 
-/usr/local/bin/docker-compose -p $job run --no-deps -v $build_cache_prefix-gradle:/home/jenkins/.gradle -v $build_cache_prefix-m2:/home/jenkins/.m2 --rm --user jenkins --entrypoint "gradle publish -x test -Penvironment=prod -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
+/usr/local/bin/docker-compose -p $job run --no-deps -v $build_cache_prefix-gradle:/home/jenkins/.gradle -v $build_cache_prefix-m2:/home/jenkins/.m2 --rm --user jenkins --entrypoint "gradle publish -x test -Partifactory=${ARTIFACTORY_BASE_URL}/remote-repos/ -Penvironment=prod -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}" my-studies-builder
 
 
