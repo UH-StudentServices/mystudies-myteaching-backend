7335cf7273a196574b476a637cd216541da24e7c
Sami Siren <siren@foofactory.fi>
2018-11-16 09:03:08 +0200

    use opintoni_artifactory_base_url instead

--- docker/ci/script/my-backend-check-pull-request.sh
+++ docker/ci/script/my-backend-check-pull-request.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./run.sh my-backend-check-pull-request "gradle test --info -Penvironment=dev -Partifactory=${ARTIFACTORY_BASE_URL}/remote-repos/ -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD} --stacktrace"
+./run.sh my-backend-check-pull-request "gradle test --info -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD} --stacktrace"
 
--- docker/ci/script/my-backend-publish.sh
+++ docker/ci/script/my-backend-publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./run.sh my-backend-publish "gradle publish -x test -Partifactory=${ARTIFACTORY_BASE_URL}/remote-repos/ -Penvironment=prod -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}"
+./run.sh my-backend-publish "gradle publish -x test -Penvironment=prod -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}"
