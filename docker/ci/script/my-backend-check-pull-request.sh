477ea9c13f47e1e0a145bf18ea05810cd9034244
Juuso Lehtinen <juuso.lehtinen@codento.com>
2019-01-03 14:49:43 +0200

    Add sonar check to pull request script

--- docker/ci/script/my-backend-check-pull-request.sh
+++ docker/ci/script/my-backend-check-pull-request.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./run.sh my-backend-check-pull-request "gradle test --info -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD} --stacktrace"
-
+./run.sh my-backend-check-pull-request "gradle test --info -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD} --stacktrace && gradle sonarqube --stacktrace -Dsonar.analysis.mode=preview -Dsonar.host.url=https://opa-ci.it.helsinki.fi/sonar/ -Dsonar.login=$SONARCUBE_TOKEN -Dsonar.github.repository=UH-StudentServices/mystudies-myteaching-backend -Dsonar.github.pullRequest=${ghprbPullId} -Dsonar.github.oauth=$SONAR_GITHUB_TOKEN"
