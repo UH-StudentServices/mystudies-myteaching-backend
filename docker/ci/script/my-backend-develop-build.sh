bf493941c12cd0bc4c22c3ee26142634d5a8377f
Juuso Lehtinen <juuso.lehtinen@codento.com>
2019-01-03 16:31:21 +0200

    Run sonar analyze for development-branch build

--- docker/ci/script/my-backend-develop-build.sh
+++ docker/ci/script/my-backend-develop-build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./run.sh my-backend-develop-build "gradle --info storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD}"
+# Sonar login $SONARQUBE_TOKEN is stored in Jenkins credentials
+
+./run.sh my-backend-develop-build "gradle --info storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Psonar=true -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD} -Dsonar.host.url=https://opa-ci.it.helsinki.fi/sonar/ -Dsonar.login=$SONARQUBE_TOKEN"
--- docker/ci/script/my-backend-sonarqube.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Sonar login $SONARQUBE_TOKEN is stored in Jenkins credentials
-
-./run.sh my-backend-sonarcube "gradle sonarqube --info -Penvironment=dev --stacktrace -Dsonar.host.url=https://opa-ci.it.helsinki.fi/sonar/ -Dsonar.login=$SONARQUBE_TOKEN"
