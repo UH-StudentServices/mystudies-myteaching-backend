f6db8f81ce28a224ec82267f8bef043d7fb19305
Juuso Lehtinen <juuso.lehtinen@codento.com>
2019-01-04 09:11:24 +0200

    Renamed variables

--- docker/ci/script/my-backend-check-pull-request.sh
+++ docker/ci/script/my-backend-check-pull-request.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Sonar login $SONARQUBE_TOKEN & github token $SONAR_GITHUB_TOKEN are stored in Jenkins credentials
+# Sonar login $sonar_mystudies_myteaching_backend_token & github token $sonar_github_plugin_token are stored in Jenkins credentials
 
-./run.sh my-backend-check-pull-request "gradle sonarqube --info -Penvironment=dev --stacktrace -Dsonar.analysis.mode=preview -Dsonar.host.url=https://opa-ci.it.helsinki.fi/sonar/ -Dsonar.login=$SONARQUBE_TOKEN -Dsonar.github.repository=UH-StudentServices/mystudies-myteaching-backend -Dsonar.github.pullRequest=${ghprbPullId} -Dsonar.github.oauth=$SONAR_GITHUB_TOKEN"
+./run.sh my-backend-check-pull-request "gradle sonarqube --info -Penvironment=dev --stacktrace -Dsonar.analysis.mode=preview -Dsonar.host.url=https://opa-ci.it.helsinki.fi/sonar/ -Dsonar.login=$sonar_mystudies_myteaching_backend_token -Dsonar.github.repository=UH-StudentServices/mystudies-myteaching-backend -Dsonar.github.pullRequest=${ghprbPullId} -Dsonar.github.oauth=$sonar_github_plugin_token"
--- docker/ci/script/my-backend-develop-build.sh
+++ docker/ci/script/my-backend-develop-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Sonar login $SONARQUBE_TOKEN is stored in Jenkins credentials
+# Sonar login $sonar_mystudies_myteaching_backend_token is stored in Jenkins credentials
 
-./run.sh my-backend-develop-build "gradle --info storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Psonar=true -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD} -Dsonar.host.url=https://opa-ci.it.helsinki.fi/sonar/ -Dsonar.login=$SONARQUBE_TOKEN"
+./run.sh my-backend-develop-build "gradle --info storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Psonar=true -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD} -Dsonar.host.url=https://opa-ci.it.helsinki.fi/sonar/ -Dsonar.login=$sonar_mystudies_myteaching_backend_token"
