0f61e07eac317d37220ed07fd5d24d44928f8e3b
Joni Korhonen <joni.korhonen@gofore.com>
2019-02-01 13:09:45 +0200

    force change detection

--- docker/ci/script/my-backend-check-pull-request.sh
+++ docker/ci/script/my-backend-check-pull-request.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Sonar login $sonar_mystudies_myteaching_backend_token & github token $sonar_github_plugin_token are stored in Jenkins credentials
-
 ./run.sh my-backend-check-pull-request "gradle sonarqube --info -Penvironment=dev --stacktrace -Dsonar.analysis.mode=preview -Dsonar.host.url=https://opa-ci.it.helsinki.fi/sonar/ -Dsonar.login=$sonar_mystudies_myteaching_backend_token -Dsonar.github.repository=UH-StudentServices/mystudies-myteaching-backend -Dsonar.github.pullRequest=${ghprbPullId} -Dsonar.github.oauth=$sonar_github_plugin_token"
