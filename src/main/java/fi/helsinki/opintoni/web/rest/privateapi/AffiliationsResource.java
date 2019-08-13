2a3bb21a098acc25879057f5395c7f6d0aa0aacc
Marko Rautajoki <markorautajoki@gmail.com>
2019-08-13 13:33:56 +0300

    Fix lint

--- src/main/java/fi/helsinki/opintoni/web/rest/privateapi/AffiliationsResource.java
+++ src/main/java/fi/helsinki/opintoni/web/rest/privateapi/AffiliationsResource.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of MystudiesMyteaching application.
+ *
+ * MystudiesMyteaching application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MystudiesMyteaching application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MystudiesMyteaching application.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fi.helsinki.opintoni.web.rest.privateapi;
 
 import com.codahale.metrics.annotation.Timed;
@@ -14,12 +31,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(RestConstants.PRIVATE_API_V1)
-public class AffiliationResource extends AbstractResource {
+public class AffiliationsResource extends AbstractResource {
 
     private final AffiliationsService affiliationsService;
 
     @Autowired
-    public AffiliationResource(AffiliationsService affiliationsService) {
+    public AffiliationsResource(AffiliationsService affiliationsService) {
         this.affiliationsService = affiliationsService;
     }
 
