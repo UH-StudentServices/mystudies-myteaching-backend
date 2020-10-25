773073268494f57cdfc4ab7f68dec52fc7867bdf
Sami Siren <siren@foofactory.fi>
2020-10-25 20:35:41 +0200

    sonarcube not working

--- docker/ci/script/my-backend-develop-build.sh
+++ docker/ci/script/my-backend-develop-build.sh
@@ -2,4 +2,4 @@
 
 # Sonar login $sonar_mystudies_myteaching_backend_token is stored in Jenkins credentials
 
-./run.sh my-backend-develop-build "gradle --info sonarqube storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Psonar=true -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD} -Dsonar.host.url=https://opa-ci.it.helsinki.fi/sonar/ -Dsonar.login=$sonar_mystudies_myteaching_backend_token"
+./run.sh my-backend-develop-build "gradle --info storeDeployInformationToFile publish --stacktrace --refresh-dependencies -Psonar=true -Penvironment=dev -Popintoni_artifactory_base_url=${ARTIFACTORY_BASE_URL} -Popintoni_artifactory_username=${ARTIFACTORY_USERNAME} -Popintoni_artifactory_password=${ARTIFACTORY_PASSWORD} -Dsonar.host.url=https://opa-ci.it.helsinki.fi/sonar/ -Dsonar.login=$sonar_mystudies_myteaching_backend_token"
